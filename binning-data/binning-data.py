b484f6bde4f88a8f670555b484142fe0c30c2dec
Beregszászi István <beistvan@gmail.com>
2023-09-20 18:03:26 +0200

    Binning Data

--- binning-data/binning-data.py
+++ binning-data/binning-data.py
@@ -18,3 +18,23 @@ print(coffee['binned_ages'].head(10))
 coffee['binned_ages'].value_counts().plot(kind='bar')
 
 plt.show()
+
+# to run type in command line:
+# python binning-data.py
+
+"""
+13
+70
+0    [40, 71)
+1    [30, 40)
+2    [20, 30)
+3    [20, 30)
+4    [20, 30)
+5    [20, 30)
+6    [20, 30)
+7    [20, 30)
+8    [20, 30)
+9    [20, 30)
+Name: binned_ages, dtype: category
+Categories (4, interval[int64, left]): [[12, 20) < [20, 30) < [30, 40) < [40, 71)]
+"""
